0f1a65d1c586a5f7198feac36fc95cd3c51d2e5d
hermannm <hermann.morkrid@gmail.com>
2022-12-03 11:33:25 +0100

    extract common code of day3 to shared module

--- src/day3/part2.rs
+++ src/day3/part2.rs
@@ -1,3 +1,5 @@
+use super::shared::{Item, Priority};
+
 #[allow(dead_code)]
 pub fn solve_puzzle() -> Result<i32, String> {
     let input = include_str!("input.txt");
@@ -47,27 +49,3 @@ fn find_badge_item(group: [String; 3]) -> Option<Item> {
 
     None
 }
-
-type Item = char;
-
-trait Priority {
-    fn get_priority(&self) -> Result<i32, String>;
-}
-
-impl Priority for Item {
-    fn get_priority(&self) -> Result<i32, String> {
-        let priority = ITEM_TYPES
-            .iter()
-            .position(|item| item == self)
-            .ok_or("Invalid item type".to_string())?;
-
-        i32::try_from(priority + 1)
-            .map_err(|_| "Could not convert priority to 32-bit integer".to_string())
-    }
-}
-
-static ITEM_TYPES: [Item; 52] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-];
